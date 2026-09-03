4c81cd5060e6256d6bb887ea4c4c871781e1bed6
Sushmita7-tech <vinubulagoud@gmail.com>
2026-09-03 17:50:21 +0000

    updated code

--- header.h
+++ header.h
@@ -1,24 +1,33 @@
 #ifndef APC_H
 #define APC_H
 
-#define FAILURE 1;
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>         //input and output/error
+#include<string.h>        //string manipulation functions
+#include<stdlib.h>       //Dynamic memory allocation functions
+#include<ctype.h>        //character classification functions for conveting digit
 
+#define SUCCESS 1
+#define FAILURE 0
 
-typedef struct node{
+typedef struct node
+{
+ unsigned char data;       //Stores a single numeric digit (0 to 9)
+ struct node *prev;    //Pointer to the previous node
+ struct node *next;    //Pointer to the next node
+}Dlist;  
 
-    int data;
-    struct node *prev;
-    struct node *next;
-}Dlist;
+int validate_args(int argc, char *argv[]);
+int  string_to_list(char *str,Dlist **head,Dlist **tail);
 
-void insert_first(Dlist **head,Dlist **tail,int data);
+int insert_at_first(Dlist **head,Dlist **tail,unsigned char data);
+int insert_at_last(Dlist **head,Dlist **tail,unsigned char data);
 void print_list(Dlist *head);
-void store_number(Dlist **head,Dlist **tail,char *str);
-void addition(Dlist *tail1,Dlist *tail2,Dlist *reshead,Dlist *restail);
-int validate(char *str);
+void free_list(Dlist **head);
 
+
+int Add(Dlist *head1,Dlist *tail,Dlist *head2, Dlist *tail2,Dlist **res_head,Dlist **res_tail);
+int sub(Dlist *head1,Dlist *tail,Dlist *head2, Dlist *tail2,Dlist **res_head,Dlist **res_tail);
+int mul(Dlist *head1,Dlist *tail,Dlist *head2, Dlist *tail2,Dlist **res_head,Dlist **res_tail);
+int Div(Dlist *head1,Dlist *tail,Dlist *head2, Dlist *tail2,Dlist **res_head,Dlist **res_tail);
 
 #endif
