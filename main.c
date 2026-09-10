cfa16ae97da50ad3ea0b9684ef35e8df0d02bafb
Sushmita7-tech <vinubulagoud@gmail.com>
2026-09-10 14:03:50 +0000

    project successfull

--- main.c
+++ main.c
@@ -10,12 +10,10 @@ int is_digit(char *str)
     if (str[0] == '\0')
         return 0;
 
-    /* Allow negative number */
     if (str[0] == '-')
     {
         i = 1;
 
-        /* Only '-' is not a valid number */
         if (str[1] == '\0')
             return 0;
     }
@@ -29,6 +27,12 @@ int is_digit(char *str)
     return 1;
 }
 
+/* Return 1 if number is negative, otherwise 0 */
+int is_negative(char *str)
+{
+    return (str[0] == '-');
+}
+
 int main(int argc, char *argv[])
 {
     char *n1;
@@ -37,14 +41,11 @@ int main(int argc, char *argv[])
 
     Dlist *head1 = NULL;
     Dlist *tail1 = NULL;
-
     Dlist *head2 = NULL;
     Dlist *tail2 = NULL;
-
     Dlist *headR = NULL;
     Dlist *tailR = NULL;
 
-    /* Check command line arguments */
     if (argc != 4)
     {
         printf("Usage: %s <number1> <operator> <number2>\n", argv[0]);
@@ -52,98 +53,182 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    /* Get values from command line */
     n1 = argv[1];
     op = argv[2][0];
     n2 = argv[3];
 
-    /* Validate numbers */
     if (!is_digit(n1) || !is_digit(n2))
     {
         printf("Invalid input: Enter numbers only\n");
         return 0;
     }
 
-    /* Validate operator */
-    if (op != '+' && op != '-' && op!='/' && op!='x')
+    if (op != '+' && op != '-' && op != '/' && op != 'x')
     {
         printf("Invalid operator\n");
         return 0;
     }
 
-    store_number(&head1, &tail1, n1);
-    store_number(&head2, &tail2, n2);
+    /* Store magnitude only; signs are handled separately. */
+    store_number(&head1, &tail1, (n1[0] == '-') ? n1 + 1 : n1);
+    store_number(&head2, &tail2, (n2[0] == '-') ? n2 + 1 : n2);
 
     switch (op)
     {
         case '+':
-            addition(tail1, tail2, &headR, &tailR);
+        {
+            int sign1 = is_negative(n1);
+            int sign2 = is_negative(n2);
+            int cmp = compare(head1, head2);
 
             printf("Result = ");
+
+            if (sign1 == sign2)
+            {
+                /* Same signs: add magnitudes */
+                addition(tail1, tail2, &headR, &tailR);
+
+                if (sign1 && headR && !(headR->data == 0 && headR->next == NULL))
+                    printf("-");
+            }
+            else
+            {
+                /* Different signs: subtract smaller magnitude from larger */
+                if (cmp == 0)
+                {
+                    printf("0\n");
+                    break;
+                }
+
+                if (cmp > 0)
+                {
+                    subtraction(head1, tail1, head2, tail2,
+                                &headR, &tailR);
+
+                    if (sign1)
+                        printf("-");
+                }
+                else
+                {
+                    subtraction(head2, tail2, head1, tail1,
+                                &headR, &tailR);
+
+                    if (sign2)
+                        printf("-");
+                }
+            }
+
             print_list(headR);
             break;
+        }
 
         case '-':
         {
-            int negative = (compare(head1, head2) < 0);
+            int sign1 = is_negative(n1);
+            int sign2 = is_negative(n2);
+            int result_negative = 0;
+            int cmp = compare(head1, head2);
 
-            subtraction(head1, tail1,head2, tail2,&headR, &tailR);
+            /*
+               Mathematical rule:
+               (+A) - (+B) = A-B
+               (+A) - (-B) = A+B
+               (-A) - (+B) = -(A+B)
+               (-A) - (-B) = B-A
+            */
+            printf("Result = ");
+
+            if (sign1 != sign2)
+            {
+                /* Different signs => magnitudes are added */
+                addition(tail1, tail2, &headR, &tailR);
+
+                result_negative = sign1;
+
+                if (result_negative && headR &&
+                    !(headR->data == 0 && headR->next == NULL))
+                    printf("-");
+
+                print_list(headR);
+            }
+            else
+            {
+                /* Same signs => subtract smaller magnitude from larger */
+                if (cmp == 0)
+                {
+                    printf("0\n");
+                }
+                else if (cmp > 0)
+                {
+                    subtraction(head1, tail1, head2, tail2,
+                                &headR, &tailR);
+
+                    result_negative = sign1;
+
+                    if (result_negative)
+                        printf("-");
+
+                    print_list(headR);
+                }
+                else
+                {
+                    subtraction(head2, tail2, head1, tail1,
+                                &headR, &tailR);
+
+                    /* If both were positive: A-B is negative.
+                       If both were negative: -A-(-B) = B-A is positive. */
+                    result_negative = !sign1;
+
+                    if (result_negative)
+                        printf("-");
+
+                    print_list(headR);
+                }
+            }
+            break;
+        }
+
+        case 'x':
+        {
+            int sign = is_negative(n1) ^ is_negative(n2);
+
+            multiplication(tail1, tail2, &headR, &tailR);
 
             printf("Result = ");
 
-            if (negative)
+            if (sign && headR && !(headR->data == 0 && headR->next == NULL))
                 printf("-");
 
             print_list(headR);
             break;
         }
-        case 'x':
-        {
-        int sign = 1;
 
-        if (n1[0] == '-')
-            sign *= -1;
-
-        if (n2[0] == '-')
-            sign *= -1;
-
-        multiplication(tail1, tail2,&headR, &tailR);
-
-        printf("Result = ");
-
-        if (sign < 0)
-            printf("-");
-
-        print_list(headR);
-        break;
-        }
         case '/':
         {
-            
-        int sign = 1;
+            int sign = is_negative(n1) ^ is_negative(n2);
 
-        if (n1[0] == '-')
-            sign *= -1;
+            division(head1, head2, &headR, &tailR);
 
-        if (n2[0] == '-')
-            sign *= -1;
+            /*
+               division() returns without creating a result
+               when divisor is zero.
+            */
+            if (headR == NULL)
+                break;
 
-        division(head1, head2,&headR, &tailR);
+            printf("Result = ");
 
-        printf("Result = ");
+            if (sign && headR && !(headR->data == 0 && headR->next == NULL))
+                printf("-");
 
-        if (sign < 0 && !(headR && headR->data == 0 && headR->next == NULL))
-        printf("-");
-
-        print_list(headR);
-        break;
-    }
+            print_list(headR);
+            break;
+        }
 
         default:
             printf("Invalid operator\n");
             return 0;
     }
-    
 
     free_list(&head1, &tail1);
     free_list(&head2, &tail2);
--- operation.c
+++ operation.c
@@ -1,7 +1,8 @@
 #include "header.h"
 
-/* ADDITION */
-void addition(Dlist *tail1, Dlist *tail2,Dlist **headR, Dlist **tailR)
+/* ADDITION - works on magnitudes only */
+void addition(Dlist *tail1, Dlist *tail2,
+              Dlist **headR, Dlist **tailR)
 {
     int carry = 0;
 
@@ -23,26 +24,19 @@ void addition(Dlist *tail1, Dlist *tail2,Dlist **headR, Dlist **tailR)
 
         carry = sum / 10;
 
-        Dlist *new = malloc(sizeof(Dlist));
-        new->data = sum % 10;
-        new->prev = NULL;
-        new->next = *headR;
-
-        if (*headR)
-            (*headR)->prev = new;
-        else
-            *tailR = new;
-
-        *headR = new;
+        insert_first(headR, tailR, sum % 10);
     }
 }
 
 
-/* SUBTRACTION */
-void subtraction(Dlist *head1, Dlist *tail1,Dlist *head2, Dlist *tail2,Dlist **headR, Dlist **tailR)
+/* SUBTRACTION - subtracts smaller magnitude from larger magnitude */
+void subtraction(Dlist *head1, Dlist *tail1,
+                 Dlist *head2, Dlist *tail2,
+                 Dlist **headR, Dlist **tailR)
 {
     int borrow = 0;
 
+    /* Always calculate larger magnitude - smaller magnitude */
     if (compare(head1, head2) < 0)
     {
         Dlist *temp;
@@ -76,23 +70,13 @@ void subtraction(Dlist *head1, Dlist *tail1,Dlist *head2, Dlist *tail2,Dlist **h
             borrow = 0;
         }
 
-        Dlist *new = malloc(sizeof(Dlist));
-
-        new->data = diff;
-        new->prev = NULL;
-        new->next = *headR;
-
-        if (*headR)
-            (*headR)->prev = new;
-        else
-            *tailR = new;
-
-        *headR = new;
+        insert_first(headR, tailR, diff);
 
         tail1 = tail1->prev;
     }
 
-    while (*headR &&(*headR)->data == 0 &&(*headR)->next)
+    /* Remove leading zeros */
+    while (*headR && (*headR)->data == 0 && (*headR)->next)
     {
         Dlist *temp = *headR;
 
@@ -103,7 +87,9 @@ void subtraction(Dlist *head1, Dlist *tail1,Dlist *head2, Dlist *tail2,Dlist **h
     }
 }
 
-void multiplication(Dlist *tail1, Dlist *tail2,Dlist **headR, Dlist **tailR)
+
+void multiplication(Dlist *tail1, Dlist *tail2,
+                     Dlist **headR, Dlist **tailR)
 {
     int arr1[1000];
     int arr2[1000];
@@ -144,46 +130,130 @@ void multiplication(Dlist *tail1, Dlist *tail2,Dlist **headR, Dlist **tailR)
     int size = len1 + len2;
 
     while (size > 1 && result[size - 1] == 0)
-    {
         size--;
-    }
 
-    /* Store result in DLL */
     for (int i = size - 1; i >= 0; i--)
-    {
         insert_last(headR, tailR, result[i]);
-    }
 }
-/* DIVISION */
-void division(Dlist *head1, Dlist *head2,Dlist **headR, Dlist **tailR)
+
+
+/* DIVISION - supports numbers of any length */
+void division(Dlist *head1, Dlist *head2,
+              Dlist **headR, Dlist **tailR)
 {
-    long long num1 = 0;
-    long long num2 = 0;
+    Dlist *p1 = head1;
+    Dlist *remainderHead = NULL;
+    Dlist *remainderTail = NULL;
 
-    
-    while (head1)
-    {
-        num1 = num1 * 10 + head1->data;
-        head1 = head1->next;
-    }
+    /* Find the tail of divisor. subtraction() needs both head and tail. */
+    Dlist *tail2 = head2;
+    while (tail2 && tail2->next)
+        tail2 = tail2->next;
 
-    while (head2)
-    {
-        num2 = num2 * 10 + head2->data;
-        head2 = head2->next;
-    }
-
-    if (num2 == 0)
+    /* Division by zero check */
+    if (head2 == NULL)
     {
         printf("Division by zero\n");
         return;
     }
 
-    long long result = num1 / num2;
+    /* Check whether divisor is actually 0 */
+    Dlist *temp = head2;
+    while (temp && temp->data == 0)
+        temp = temp->next;
 
-    char str[100];
+    if (temp == NULL)
+    {
+        printf("Division by zero\n");
+        return;
+    }
 
-    sprintf(str, "%lld", result);
+    /*
+     * Long division:
+     * Bring down one digit at a time.
+     * For every digit, find the quotient digit (0 to 9)
+     * by repeatedly subtracting the divisor from remainder.
+     */
+    while (p1)
+    {
+        /* Bring down the next digit */
+        insert_last(&remainderHead, &remainderTail, p1->data);
 
-    store_number(headR, tailR, str);
+        /* Remove leading zeros from remainder */
+        while (remainderHead &&
+               remainderHead->data == 0 &&
+               remainderHead->next)
+        {
+            Dlist *del = remainderHead;
+            remainderHead = remainderHead->next;
+            remainderHead->prev = NULL;
+            free(del);
+        }
+
+        int quotientDigit = 0;
+
+        /*
+         * A quotient digit can only be 0..9.
+         * Subtract divisor while remainder >= divisor.
+         */
+        while (remainderHead &&
+               compare(remainderHead, head2) >= 0 &&
+               quotientDigit < 10)
+        {
+            Dlist *newRemHead = NULL;
+            Dlist *newRemTail = NULL;
+
+            subtraction(remainderHead, remainderTail,
+                        head2, tail2,
+                        &newRemHead, &newRemTail);
+
+            /* Free old remainder */
+            while (remainderHead)
+            {
+                Dlist *del = remainderHead;
+                remainderHead = remainderHead->next;
+                free(del);
+            }
+
+            remainderHead = newRemHead;
+            remainderTail = newRemTail;
+
+            quotientDigit++;
+        }
+
+        insert_last(headR, tailR, quotientDigit);
+
+        p1 = p1->next;
+    }
+
+    /*
+     * Remove leading zeros from quotient.
+     * Keep one zero if the complete quotient is zero.
+     */
+    while (*headR && (*headR)->data == 0 && (*headR)->next)
+    {
+        Dlist *del = *headR;
+        *headR = (*headR)->next;
+        (*headR)->prev = NULL;
+        free(del);
+    }
+
+    /* Free remainder */
+    while (remainderHead)
+    {
+        Dlist *del = remainderHead;
+        remainderHead = remainderHead->next;
+        free(del);
+    }
+
+    /*
+     * tailR may have become invalid only if the result was empty.
+     * Normally the result always contains at least one digit.
+     */
+    if (*headR)
+    {
+        *tailR = *headR;
+        while ((*tailR)->next)
+            *tailR = (*tailR)->next;
+    }
 }
